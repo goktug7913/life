ab5ea6e4d684d7e365732898183b4c228f085691
goktug7913 <goktug7913@gmail.com>
2022-08-11 20:36:31 +0300

    player controls refactoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,15 @@ public class PlayerController : MonoBehaviour
     public GameObject _Camera;
 
     public float moveSpeed = 3f;
-    public float zoomSpeed = 3f;
+    public float zoomSpeed = 5000f;
     public float orbitSpeed = 1f;
     private float orbitMultiplier = 1000f;
+    private float moveMultiplier = 10f;
     public bool invertY = true;
+
+    public bool canMove = true;
+    public bool canZoom = true;
+    public bool canOrbit = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +29,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // WASD movement.
+        CameraMove();
+        CameraOrbit();
+        ScrollZoom();
+    }
+
+    void CameraMove()
+    {
+        if (!canMove){return;} // If can't move, return.
+        
+        // WASD movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        PlayerRoot.transform.position += movement * Time.deltaTime * moveSpeed;
 
-        
+        if (moveHorizontal == 0 && moveVertical == 0){return;} // If no input, return.
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // rotate movement with camera.
+        movement = Quaternion.Euler(0, CameraRoot.transform.rotation.eulerAngles.y, 0) * movement;
+
+        // add modifiers and move.
+        movement = movement * moveSpeed * Time.deltaTime;
+        PlayerRoot.transform.Translate(movement);
+    }
+
+    void CameraOrbit()
+    {
+        if (!canOrbit){return;} // If can't orbit, return.
+
         if (Input.GetMouseButton(2))
         {
             // get cursor position
@@ -49,13 +77,22 @@ public class PlayerController : MonoBehaviour
             // we need to zero out Z axis... (fix later)
             CameraRoot.transform.rotation = Quaternion.Euler(CameraRoot.transform.rotation.eulerAngles.x, CameraRoot.transform.rotation.eulerAngles.y, 0);
         }
+    }
 
-        // scroll zoom
+    void ScrollZoom()
+    {
+        if (!canZoom){return;} // If can't zoom, return.
+
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0)
-        {
-            // lerp the camera's field of view to a new value.
-            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, Camera.main.fieldOfView - scroll * zoomSpeed, Time.deltaTime);
-        }
+
+        if (scroll == 0){return;} // if no scroll, return.
+
+        // change camera distance based on scroll.
+        float zoom = scroll * zoomSpeed;
+
+        // move camera
+        _Camera.transform.Translate(0, 0, zoom);
     }
 }
+
+
